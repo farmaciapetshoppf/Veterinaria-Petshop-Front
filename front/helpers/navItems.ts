c6835a2c55af2900077d1dd18073002833b1f7fa
matias-bascunan <matifb22@gmail.com>
2025-11-25 19:28:55 -0300

    "init-navbar"

---
+++ front/helpers/navItems.ts
@@ -0,0 +1,26 @@
+import {PATHROUTES} from "../helpers/pathRoutes";
+
+export const navItems = [
+    
+    {
+        id:1,
+        nameToRender:"Store",
+        route: PATHROUTES.STORE,
+    },
+    {
+        id:2,
+        nameToRender:"Historia",
+        route: PATHROUTES.HISTORIA,},
+
+    {
+         id:3,
+        nameToRender:"Nuestro equipo",
+        route: PATHROUTES.NOSOTROS
+     },
+    {
+        id:4,
+        nameToRender:"Mi perfil",
+        route: PATHROUTES.PERFIL
+
+    },
+    ];
