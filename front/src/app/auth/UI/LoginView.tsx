4b433b476f16d89fb0fb21a08ff3332ac00f6d17
Adrian <adrianmespindola@gmail.com>
2025-11-26 22:18:32 -0300

    Empezando con Google Auth

--- front/src/app/auth/UI/LoginView.tsx
+++ front/src/app/auth/UI/LoginView.tsx
@@ -6,19 +6,23 @@ import FieldFormikCustom from '../../components/FieldFormikCustom/FieldFormikCus
 import SubmitFormikButton from '../../components/SubmitFormikButton/SubmitFormikButton'
 import { validateLoginForm } from '@/src/utils/validate'
 import Link from 'next/link'
+import { signIn, useSession } from 'next-auth/react'
 
 function LoginView() {
+
+    const {data: session}=  useSession()
+
     return (
         <div className='flex flex-col items-center justify-center bg-white'>
 
             <p className='text-5xl mt-4 text-black'>Ingresá</p>
 
             <p className='text-black mt-4'>¿No tienes cuenta?
-                <Link href="/auth/register" 
-                className='text-blue-500 cursor-pointer'> ¡Registrate!</Link>
+                <Link href="/auth/register"
+                    className='text-blue-500 cursor-pointer'> ¡Registrate!</Link>
             </p>
 
-            
+
             <Formik
                 initialValues={{ email: '', password: '' }}
                 validationSchema={validateLoginForm}
@@ -35,16 +39,25 @@ function LoginView() {
             }} */
             >
                 {({ errors }) => (
-                    <Form className="flex flex-col justify-between my-6 border-2 p-7">
+                    <Form className="flex flex-col justify-between my-6 border-2 border-gray-300 p-7">
 
                         <FieldFormikCustom label="Email" nameField="email" type="email" placeholder="johnHandcock@mail.com" />
-                        
+
                         <FieldFormikCustom label="Contraseña" nameField="password" type="password" placeholder="******" />
-                        
+
                         <SubmitFormikButton text="Ingresar" disabled={errors.email || errors.password ? true : false} />
+
+                        <button onClick={() => signIn()} className='bg-sky-600 p-3 w-1/2 flex self-center justify-center'>
+                            Sign In with Google
+                        </button>
                     </Form>
                 )}
+
+
+
             </Formik>
+
+
         </div>
     )
 }
